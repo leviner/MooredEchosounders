f8d78b0f74d585fd86163eebfa004256facef265
leviner <leviner@uw.edu>
2021-07-27 22:52:35 -0700

    modify printout

--- code/CTDSpeedOfSound.py
+++ code/CTDSpeedOfSound.py
@@ -12,17 +12,17 @@ for i in [0,1,2]:
     a = pd.read_csv(tsFiles[i])
     a['time (UTC)'] = pd.to_datetime(a['time (UTC)'])
     y = a[a['time (UTC)'] < pd.to_datetime(dt[i]).tz_localize('UTC')]
-    print('Site C',str(site[i]),'2017-2018: \n - T:',round(y['temperature (degree_C)'].mean(),3),'\n - S:',round(y['salinity'].mean(),3),
-          '\n - C:',round(seawater.eos80.svel(y['salinity'].mean(),y['temperature (degree_C)'].mean(),0),3),
-          '(',round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],0).min(),3),
-          round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],0).max(),3),')')
+    print('Site C',str(site[i]),'2017-2018 (depth = '+str(y['depth (m)'].unique()[0])+'): \n - T:',round(y['temperature (degree_C)'].mean(),3),'\n - S:',round(y['salinity'].mean(),3),
+          '\n - C:',round(seawater.eos80.svel(y['salinity'].mean(),y['temperature (degree_C)'].mean(),y['depth (m)'].unique()[0]),3),
+          '(',round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],y['depth (m)'].unique()[0]).min(),3),
+          round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],y['depth (m)'].unique()[0]).max(),3),')')
     y['temperature (degree_C)'].plot(ax=fig.axes[i],secondary_y=False,color='skyblue',linewidth=2, label = 'Temperature')
     y['salinity'].plot(ax=fig.axes[i],secondary_y=True,color='mediumpurple',linewidth=2, label = 'Salinity')
     y = a[a['time (UTC)'] > pd.to_datetime(dt[i]).tz_localize('UTC')]
-    print('Site C',str(site[i]),'2018-2019: \n - T:',round(y['temperature (degree_C)'].mean(),3),'\n - S:',round(y['salinity'].mean(),3),
-          '\n - C:',round(seawater.eos80.svel(y['salinity'].mean(),y['temperature (degree_C)'].mean(),0),3),
-          '(',round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],0).min(),3),
-          round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],0).max(),3),')')
+    print('Site C',str(site[i]),'2018-2019 (depth = '+str(y['depth (m)'].unique()[0])+'): \n - T:',round(y['temperature (degree_C)'].mean(),3),'\n - S:',round(y['salinity'].mean(),3),
+          '\n - C:',round(seawater.eos80.svel(y['salinity'].mean(),y['temperature (degree_C)'].mean(),y['depth (m)'].unique()[0]),3),
+          '(',round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],y['depth (m)'].unique()[0]).min(),3),
+          round(seawater.eos80.svel(y['salinity'],y['temperature (degree_C)'],y['depth (m)'].unique()[0]).max(),3),')')
     y['temperature (degree_C)'].plot(ax=fig.axes[i],secondary_y=False,color='deepskyblue',linewidth=2, label = 'Temperature')
     y['salinity'].plot(ax=fig.axes[i],secondary_y=True,color='purple',linewidth=2, label = 'Salinity')
     plt.title('Purple = Salinity         Blue = Temperature')
